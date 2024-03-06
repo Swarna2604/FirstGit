1932f73049cd46c490d9b2c3505e91644032a24a
Kumari <ganta-swarna.kumari@capgemini.com>
2024-03-06 11:22:17 +0530

    Hello Git

---
+++ GITDemo/src/com/cg/Demo/FirstGIT.java
@@ -0,0 +1,11 @@
+package com.cg.Demo;
+
+public class FirstGIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to GIT");
+
+	}
+
+}
